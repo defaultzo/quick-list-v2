9d38a422101172ad9e1f3c467df52f1030a35d8d
deafultzo <leonardoaraujomarques611@gmail.com>
2026-04-13 16:03:59 -0300

    troca de pastas + li trocado por checkbocx com sucesso

--- quickl-ist-v2/script.js
+++ quickl-ist-v2/script.js
@@ -9,15 +9,16 @@ const input = document.querySelector('input')
 //Variavel da lista
 const list = document.querySelector('ul')
 
+//Variavel do checkbox
+const checkbox = document.querySelector('.checkbox-input')
+
 //Variavel da seção de alerta de item removido
 const removeAlert = document.querySelector('.removeItem')
 
-//O ícone de fechar é a última imagem dentro do alerta
-const closeAlertIcon = removeAlert.querySelector('img[src*="iconDelete"]')
 
 /*
 //Só para verificar se as variaveis estão pegando os elementos certos
-console.log(form, input, list)
+console.log(form, input, list, checkbox)
 */
 
 
@@ -31,50 +32,67 @@ form.addEventListener('submit', event =>{
     //Verifica se o valor que o usuário digitou não é vazio
     if(itemValue !== ''){
         //Cria o item da lista
-        const newItem = document.createElement('li')
-        //Adiciona a classe 'itens' ao item da lista
-        newItem.classList.add('itens')
+        const newItemLi = document.createElement('li')
 
-        //Adiciona o conteudo HTML do item
-        newItem.innerHTML = `
-        <img src="./assets/iconCheckBoxDefault.svg" alt="checkbox" class="icon1">
-        <p>${itemValue}</p>
-        <img src="./assets/iconTrash.svg" alt="lixeira" class="icon2">
+        //Cria um ID único para o checkbox usando a data e hora atual
+        const itemId = `check-${Date.now()}`;
+
+        //Adiciona a classe 'itens' ao item da lista
+        newItemLi.classList.add('itens')
+
+        //Adiciona o conteudo HTML do item, isso tudo fica dentro da <li> que eu criei
+        newItemLi.innerHTML = `
+        <input type="checkbox" id="${itemId}" class="checkbox-input">
+            <label for="${itemId}" class="checkbox-label">
+                <span class="custom-checkbox"></span>
+                <p>${itemValue}</p>
+                </label>
+                <img src="assets/iconTrash.svg" class="icon2">
         `
         //Adiciona o item criado à lista apos o ultimo item, elemento filho da section
-        list.appendChild(newItem)
+        list.appendChild(newItemLi)
 
         //Limpa o valor que o usuário digitou no input, tornando ele vazio de novo
         input.value = ''
+
         //Deixa o foco no input de novo
         input.focus()
 
         //Variaveis dos icones de cada item da lista
-        const checkIcon = newItem.querySelector('.icon1')//Imagem do checkbox
-        const trashIcon = newItem.querySelector('.icon2')//Imagem da lixeira
+        const checkIcon = newItemLi.querySelector('.custom-checkbox')//Imagem do checkbox
+        const trashIcon = newItemLi.querySelector('.icon2')//Imagem da lixeira
 
-        //Evento de marcar e descarmar o icone de checkbox
+        /*
+        Outro jeito que eu pensei de fazer o check do checkbox, mas acabei fazendo de outra maneira
         checkIcon.addEventListener('click', () =>{
-        //Troca a classe do checkbox para a classe que tem a imagem do checkbox selecionado, e vice-versa
-        checkIcon.classList.toggle('check-active')
+            newItemLi.classList.toggle('checked')
         })
-
+        console.log(checkIcon)
+        */
         //Evento de excluir o item da lista
         trashIcon.addEventListener('click', () =>{
         // O comando .remove() exclui o elemento inteiro (o <li> que eu criei) do site
-        newItem.remove()
+        newItemLi.remove()
+
         //Essa variavel já tinha sido criada lá em cima
         //Exibe o alerta de item removido
         removeAlert.style.display = 'flex'
         })
 
+        //O ícone de fechar é a última imagem dentro do alerta, então eu busco ele usando querySelector dentro da classe removeAlert
+        const closeAlertIcon = removeAlert.querySelector('img[src*="iconDelete"]')
+
         //Quando clicar no "X" do alerta, ele some de novo
         closeAlertIcon.addEventListener('click', () => {
+
         //Adiciona a classe 'icon3' ao ícone de fechar para aplicar o estilo de cursor pointer
         closeAlertIcon.classList.toggle('icon3')
+        
         //Esconde o alerta de item removido
         removeAlert.style.display = 'none';
         })
     }
+
+    const btnClear = document.getElementById('btn-limpar')
 })
 
